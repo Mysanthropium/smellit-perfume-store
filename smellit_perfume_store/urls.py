2a1e60eada40255d2eef023bf093ac4dc51a6219
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-07-27 12:03:20 +0000

    Added products views and templates

--- smellit_perfume_store/urls.py
+++ smellit_perfume_store/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # This gives me all the urls I need for allauth to work
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
