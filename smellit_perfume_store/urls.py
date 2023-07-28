2658d825478b024737f7d55bd26e8913e66516f0
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-07-28 09:54:58 +0000

    Added shopping bag app

--- smellit_perfume_store/urls.py
+++ smellit_perfume_store/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')), # This gives me all the urls I need for allauth to work
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
