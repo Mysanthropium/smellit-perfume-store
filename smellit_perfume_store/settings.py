9b647933e25874e792077fa371c98712438dad7e
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-08-02 09:54:25 +0000

    Removed secret key and set debug

--- smellit_perfume_store/settings.py
+++ smellit_perfume_store/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@mf3$is(6535dy7muldr!+f%1jo!xq-v%5stk8%9atcfp%a4&c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['smellit-perfume-store-8571815b705d.herokuapp.com', 'localhost']
 
