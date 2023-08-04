95ffed102b0a0768b97a0bc4acee1ca2852095fd
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-08-04 09:45:25 +0000

    added add and update blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogView, PostDetailView
+from .views import BlogView, PostDetailView, AddPostView, UpdatePostView
 
 urlpatterns = [
     path('', BlogView.as_view(), name="blog"),
     path('article/<int:pk>', PostDetailView.as_view(), name="post_detail"),
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('edit_post/<int:pk>', UpdatePostView.as_view(), name="edit_post"),
 ]
